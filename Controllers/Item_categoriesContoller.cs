f4012084a389a4080303414a3290d070e5508a14
Slaviann <zionist@gmail.com>
2018-02-01 20:01:33 +0300

    basic CRUD

--- Controllers/Item_categoriesContoller.cs
+++ Controllers/Item_categoriesContoller.cs
@@ -10,68 +10,87 @@ namespace lagoon_back.Controllers
 {
     // /admin/api/item_categories
     [Route("admin/api/[controller]")]
+    [Authorize]
     public class Item_categoriesController : Controller
     {
 
-    //   private readonly ILoggerFactory loggerFactory;
-     //  private readonly ILogger logger;
-       private LagoonContext lagoonContext;
+        //   private readonly ILoggerFactory loggerFactory;
+        //  private readonly ILogger logger;
+        private LagoonContext lagoonContext;
 
-        public Item_categoriesController(LagoonContext lagoonContext) {
+        public Item_categoriesController(LagoonContext lagoonContext)
+        {
             this.lagoonContext = lagoonContext;
         }
 
 
-        // GET admin/api/item_Categories
+        // GET all admin/api/item_Categories
         [HttpGet]
-        [Authorize]
         public List<ItemCategory> Get()
         {
-             using (var db = this.lagoonContext)
+            using (var db = this.lagoonContext)
             {
                 var itemCategories = db.ItemCategory
                     //.Where(b => b.Rating > 3)
                     //.OrderBy(c => c.Name)
                     .ToList();
                 return itemCategories;
-                
+
             }
 
         }
 
-        // GET api/values/5
+        // get one
         [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-       
-        public List<ItemCategory> Post([FromBody]string value)
+        public ItemCategory Get(int id)
         {
             using (var db = this.lagoonContext)
             {
-                var blogs = db.ItemCategory
-                    //.Where(b => b.Rating > 3)
-                    //.OrderBy(c => c.Name)
-                    .ToList();
-                return blogs;
+                var itemCategory = db.ItemCategory
+                .Where(c => c.Id == id).First();
+                return itemCategory;
+
             }
-
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        // create
+        [HttpPost]
+        public ItemCategory Post([FromBody]ItemCategory value)
         {
+            value.ImagePath = Guid.NewGuid().ToString();
+            using (var db = this.lagoonContext)
+            {
+                db.Add(value);
+                db.SaveChanges();
+            }
+            return value;
+
         }
 
-        // DELETE api/values/5
+        // update
+        [HttpPut]
+        public ItemCategory Put([FromBody]ItemCategory value)
+        {
+
+            using (var db = this.lagoonContext)
+            {
+                //var record = db.ItemCategory.Single(c => c.Id == id );
+                db.Update(value);
+                db.SaveChanges();
+            }
+            return value;
+        }
+
+
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            using (var db = this.lagoonContext)
+            {
+                var record = db.ItemCategory.Single(c => c.Id == id);
+                db.ItemCategory.Remove(record);
+                db.SaveChanges();
+            }
         }
     }
 }
